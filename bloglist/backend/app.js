99aadaa45f5797a6ff7fc3fd5e0335bd7bf0efa5
ebrahim95 <ebrahim.akh95@gmail.com>
2023-03-01 00:11:23 -0600

    uploaded blog to fly

--- bloglist/backend/app.js
+++ bloglist/backend/app.js
@@ -21,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 app.use(middleware.requestLogger)
--- bloglist/backend/utils/config.js
+++ bloglist/backend/utils/config.js
@@ -5,7 +5,7 @@ const MONGODB_URI = process.env.NODE_ENV === ('test' || 'development')
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 const SECRET = process.env.SECRET
 
 module.exports = {
