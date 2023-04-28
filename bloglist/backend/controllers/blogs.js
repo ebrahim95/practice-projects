16aa44eca752141f71853494697d01bb26312c6f
ebrahim95 <ebrahim.akh95@gmail.com>
2023-04-28 02:19:42 -0500

    fixed deleting blogs from User model

--- bloglist/backend/controllers/blogs.js
+++ bloglist/backend/controllers/blogs.js
@@ -1,97 +1,101 @@
-const blogsRouter = require('express').Router()
-const JsonWebToken = require('jsonwebtoken')
-const Blog = require('../models/blog')
-const User = require('../models/user')
-const { userExtractor } = require('../utils/middleware')
-const { SECRET } = require('../utils/config')
+const blogsRouter = require("express").Router();
+const JsonWebToken = require("jsonwebtoken");
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const { userExtractor } = require("../utils/middleware");
+const { SECRET } = require("../utils/config");
 
 const findUserByToken = async (token) => {
-  const decodedToken = JsonWebToken.verify(token, SECRET)
-  const user = await User.findById(decodedToken.id)
-  return user
-}
+  const decodedToken = JsonWebToken.verify(token, SECRET);
+  const user = await User.findById(decodedToken.id);
+  console.log(token);
+  return user;
+};
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  response.status(201).json(blogs)
-})
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.status(201).json(blogs);
+});
 
-blogsRouter.get('/:id', async (request, response) => {
-  const id = request.params.id
-  const blog = await Blog.findById(id)
-  response.status(201).json(blog)
-})
+blogsRouter.get("/:id", async (request, response) => {
+  const id = request.params.id;
+  const blog = await Blog.findById(id);
+  response.status(201).json(blog);
+});
 
-blogsRouter.post('/', userExtractor, async (request, response) => {
-  let { title, author, url, likes } = request.body
-  const user = request.user
+blogsRouter.post("/", userExtractor, async (request, response) => {
+  let { title, author, url, likes } = request.body;
+  const user = request.user;
   if (!likes) {
-    likes = 0
+    likes = 0;
   }
 
   if (!url || !title) {
-    response.status(400).end()
+    response.status(400).end();
   }
-
+  //FIX line 41 bug - because the user does state does not update;
+  console.log(user);
   const blog = new Blog({
     title,
     author,
     url,
     likes,
-    user: user._id
-  })
+    user: user._id,
+  });
 
-  const savedBlog = await blog.save()
-  user.blogs = [...user.blogs, savedBlog._id]
-  await user.save()
-  response.status(201).json(savedBlog)
-})
+  const savedBlog = await blog.save();
+  user.blogs = [...user.blogs, savedBlog._id];
+  await user.save();
+  response.status(201).json(savedBlog);
+});
 
-blogsRouter.post('/:id/comments', async (request, response) => {
-  const id = request.params.id
-  const blog = await Blog.findById(id)
-  const { comment } = request.body
-  blog.comments = [...blog.comments, comment]
+blogsRouter.post("/:id/comments", async (request, response) => {
+  const id = request.params.id;
+  const blog = await Blog.findById(id);
+  const { comment } = request.body;
+  blog.comments = [...blog.comments, comment];
 
-  await blog.save()
-  response.status(201).json('hello')
+  await blog.save();
+  response.status(201).json(comment);
+});
 
-})
-
-blogsRouter.delete('/:id', async (request, response) => {
-  const id = request.params.id
-  const user = await findUserByToken(request.token)
-  const blogUser = await Blog.findById(id)
+blogsRouter.delete("/:id", async (request, response) => {
+  const id = request.params.id;
+  const user = await findUserByToken(request.token);
+  const blogUser = await Blog.findById(id);
   if ((user.id.toString() === blogUser.user.toString())) {
-    await Blog.findByIdAndRemove(id)
-    return response.status(204).end()
+    await Blog.findByIdAndRemove(id);
+
+    user.blogs = user.blogs.filter((blogId) => {
+      if (blogId.toString() !== id) {
+        return blogId;
+      }
+    });
+    await user.save();
+    console.log(user.blogs);
+    return response.status(204).end();
   } else {
     response.status(401).json({
-      error: 'wrong user'
-    })
+      error: "wrong user",
+    });
   }
+});
 
-
-})
-
-blogsRouter.put('/:id', async (request, response, next) => {
-  const id = request.params.id
-  const blog = request.body
+blogsRouter.put("/:id", async (request, response, next) => {
+  const id = request.params.id;
+  const blog = request.body;
 
   try {
     const updatedBlog = await Blog.findByIdAndUpdate(id, blog, {
       new: true,
       runValidators: true,
-      context: 'query'
-    })
+      context: "query",
+    });
 
-    response.json(updatedBlog)
-
+    response.json(updatedBlog);
   } catch (expection) {
-    next(expection)
+    next(expection);
   }
-}
-)
+});
 
-
-module.exports = blogsRouter
+module.exports = blogsRouter;
